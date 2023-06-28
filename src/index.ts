24b9b7c8c3bfe4a2748b327d9799fc56600c0f1b
terra456 <terra.456@yandex.ru>
2023-06-28 22:46:04 +0500

    feat: install eslint, prettier

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import { createServer as createServerHttp } from 'http';
 const PORT = 3000;
 
 const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
+    res.end('Request accepted')
 });
 
 myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
+      console.log(`Server is listening on port ${PORT}`);
+console.log('To terminate it, use Ctrl+C combination');
 });
