d1e509b436bcf07cff68161a621f7aa35b59b955
terra456 <terra.456@yandex.ru>
2023-06-28 21:59:44 +0500

    feat: install ts and nodemon

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { createServer as createServerHttp } from 'http';
+
+const PORT = 3000;
+
+const myServer = createServerHttp((_, res) => {
+    res.end('Request accepted');
+});
+
+myServer.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+    console.log('To terminate it, use Ctrl+C combination');
+});
